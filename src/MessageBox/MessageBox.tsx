1229fdb991d32c5c543164f1ea02321e4950b885
monikap0612 <monikap0612@gmail.com>
2022-10-12 19:02:02 +0530

    Reply msg popup UI

--- src/MessageBox/MessageBox.tsx
+++ src/MessageBox/MessageBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './MessageBox.css'
 
 import PhotoMessage from '../PhotoMessage/PhotoMessage'
@@ -11,12 +11,15 @@ import MeetingMessage from '../MeetingMessage/MeetingMessage'
 import VideoMessage from '../VideoMessage/VideoMessage'
 import AudioMessage from '../AudioMessage/AudioMessage'
 import MeetingLink from '../MeetingLink/MeetingLink'
+import { BsThreeDotsVertical } from "react-icons/bs"
 
 import Avatar from '../Avatar/Avatar'
+import { BsReply } from "react-icons/bs"
+import { TbMessages } from "react-icons/tb"
 
-import { RiShareForwardFill } from 'react-icons/ri'
+import { RiShareForwardFill, RiCloseLine } from 'react-icons/ri'
 import { IoIosDoneAll } from 'react-icons/io'
-import { MdAccessTime, MdCheck, MdMessage, MdDelete, MdBlock, MdDoneAll } from 'react-icons/md'
+import { MdAccessTime, MdCheck, MdMessage, MdDelete, MdBlock, MdDoneAll, MdContentCopy } from 'react-icons/md'
 
 import { format } from 'timeago.js'
 
@@ -26,6 +29,8 @@ import { MessageBoxType } from '../type'
 const MessageBox: React.FC<MessageBoxType> = ({ focus = false, notch = true, ...props }) => {
   const prevProps = useRef(focus)
   const messageRef = useRef<HTMLDivElement>(null)
+
+  const [popupData, setPopUpData] = useState(false)
 
   var positionCls = classNames('rce-mbox', { 'rce-mbox-right': props.position === 'right' })
   var thatAbsoluteTime =
@@ -46,174 +51,216 @@ const MessageBox: React.FC<MessageBoxType> = ({ focus = false, notch = true, ...
     prevProps.current = focus
   }, [focus, prevProps])
 
-  return (
-    <div ref={messageRef} className={classNames('rce-container-mbox', props.className)} onClick={props.onClick}>
-      {/* {props.renderAddCmp instanceof Function && props.renderAddCmp()} */}
-      {props.type === 'system' ? (
-        <SystemMessage {...props} focus={focus} notch={notch} />
-      ) : (
-        <div
-          className={classNames(
-            positionCls,
-            { 'rce-mbox--clear-padding': thatAbsoluteTime },
-            { 'rce-mbox--clear-notch': !notch },
-            { 'message-focus': focus }
-          )}
-        >
-          <div className='rce-mbox-body' onContextMenu={props.onContextMenu}>
-            {!props.retracted && props.forwarded === true && (
-              <div
-                className={classNames(
-                  'rce-mbox-forward',
-                  { 'rce-mbox-forward-right': props.position === 'left' },
-                  { 'rce-mbox-forward-left': props.position === 'right' }
-                )}
-                onClick={props.onForwardClick}
-              >
-                <RiShareForwardFill />
-              </div>
-            )}
+  const onClickBtnText = () => {
+    setPopUpData(true)
+  }
 
-            {!props.retracted && props.replyButton === true && (
+  const onClickCloseIcon = () => {
+    setPopUpData(false)
+  }
+
+  return (
+    <>
+      {popupData ?
+        <div className='ctp-model-main-wrapper'>
+          <div className="ctp-model-main-wrapper-inner">
+            <div className="ctp-model-file-wrapper">
+              <div className="ctp-model-close-icon" onClick={() => onClickCloseIcon()}>
+                <RiCloseLine />
+              </div>
+              <div className="rpu-model-save-as-icon">
+                <TbMessages />
+              </div>
+              <div className="ctp-dispaly-folder-ui-wrapper amm-dispaly-folder-ui-wrapper">
+                <div className="ctp-folder-name" onClick={props.onReplyClick}>
+                  <div className="stp-folder-name-right" >
+                    <span className='stp-folder-name-icon stp-folder-name-icon1'>
+                      <BsReply />
+                    </span>
+                    Reply To
+                  </div>
+                </div>
+                <div className="ctp-folder-name">
+                  <div className="stp-folder-name-right"
+                   onClick={props.onForwardClick}
+                   >
+                    <span className='stp-folder-name-icon'>
+                      <MdContentCopy />
+                    </span>
+                    Copy To
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> : null
+      }
+      <div ref={messageRef} className={classNames('rce-container-mbox', props.className)} onClick={props.onClick}>
+        {/* {props.renderAddCmp instanceof Function && props.renderAddCmp()} */}
+        {props.type === 'system' ? (
+          <SystemMessage {...props} focus={focus} notch={notch} />
+        ) : (
+          <div
+            className={classNames(
+              positionCls,
+              { 'rce-mbox--clear-padding': thatAbsoluteTime },
+              { 'rce-mbox--clear-notch': !notch },
+              { 'message-focus': focus }
+            )}
+          >
+            <div className='rce-mbox-body' onContextMenu={props.onContextMenu}>
+              {!props.retracted && props.forwarded === true && (
+                <div
+                  className={classNames(
+                    'rce-mbox-forward',
+                    { 'rce-mbox-forward-right': props.position === 'left' },
+                    { 'rce-mbox-forward-left': props.position === 'right' }
+                  )}
+                  onClick={props.onForwardClick}
+                >
+                  <RiShareForwardFill />
+                </div>
+              )}
+
               <div
                 className={
                   props.forwarded !== true
                     ? classNames(
-                        'rce-mbox-forward',
-                        { 'rce-mbox-forward-right': props.position === 'left' },
-                        { 'rce-mbox-forward-left': props.position === 'right' }
-                      )
+                      'rce-mbox-forward',
+                      { 'rce-mbox-forward-right': props.position === 'left' },
+                      { 'rce-mbox-forward-left': props.position === 'right' }
+                    )
                     : classNames(
-                        'rce-mbox-forward',
-                        { 'rce-mbox-reply-btn-right': props.position === 'left' },
-                        { 'rce-mbox-reply-btn-left': props.position === 'right' }
-                      )
+                      'rce-mbox-forward',
+                      { 'rce-mbox-reply-btn-right': props.position === 'left' },
+                      { 'rce-mbox-reply-btn-left': props.position === 'right' }
+                    )
                 }
-                onClick={props.onReplyClick}
+                onClick={() => onClickBtnText()}
               >
-                <MdMessage />
+                <BsThreeDotsVertical />
               </div>
-            )}
 
-            {!props.retracted && props.removeButton === true && (
-              <div
-                className={
-                  props.forwarded === true
-                    ? classNames(
+              {!props.retracted && props.removeButton === true && (
+                <div
+                  className={
+                    props.forwarded === true
+                      ? classNames(
                         'rce-mbox-remove',
                         { 'rce-mbox-remove-right': props.position === 'left' },
                         { 'rce-mbox-remove-left': props.position === 'right' }
                       )
-                    : classNames(
+                      : classNames(
                         'rce-mbox-forward',
                         { 'rce-mbox-reply-btn-right': props.position === 'left' },
                         { 'rce-mbox-reply-btn-left': props.position === 'right' }
                       )
-                }
-                onClick={props.onRemoveMessageClick}
-              >
-                <MdDelete />
-              </div>
-            )}
-
-            {(props.title || props.avatar) && (
-              <div
-                style={{ ...(props.titleColor && { color: props.titleColor }) }}
-                onClick={props.onTitleClick}
-                className={classNames('rce-mbox-title', {
-                  'rce-mbox-title--clear': props.type === 'text',
-                })}
-              >
-                {props.avatar && <Avatar letterItem={props.letterItem} src={props.avatar} />}
-                {props.title && <span>{props.title}</span>}
-              </div>
-            )}
-
-            {props.reply && <ReplyMessage onClick={props.onReplyMessageClick} {...props.reply} />}
-
-            {props.type === 'text' && (
-              <div
-                className={classNames('rce-mbox-text', {
-                  'rce-mbox-text-retracted': props.retracted,
-                  'left': props.position === 'left',
-                  'right': props.position === 'right',
-                })}
-              >
-                {props.retracted && <MdBlock />}
-                {props.text}
-              </div>
-            )}
-
-            {props.type === 'location' && <LocationMessage focus={focus} notch={notch} {...props} />}
-
-            {props.type === 'photo' && <PhotoMessage focus={focus} notch={notch} {...props} />}
-
-            {props.type === 'video' && <VideoMessage focus={focus} notch={notch} {...props} />}
-
-            {props.type === 'file' && <FileMessage focus={focus} notch={notch} {...props} />}
-
-            {props.type === 'spotify' && <SpotifyMessage focus={focus} notch={notch} {...props} />}
-
-            {props.type === 'meeting' && <MeetingMessage focus={focus} notch={notch} {...props} />}
-            {props.type === 'audio' && <AudioMessage focus={focus} notch={notch} {...props} />}
-
-            {props.type === 'meetingLink' && (
-              <MeetingLink focus={focus} notch={notch} {...props} actionButtons={props?.actionButtons} />
-            )}
-
-            <div
-              className={classNames(
-                'rce-mbox-time',
-                { 'rce-mbox-time-block': thatAbsoluteTime },
-                { 'non-copiable': !props.copiableDate }
+                  }
+                  onClick={props.onRemoveMessageClick}
+                >
+                  <MdDelete />
+                </div>
               )}
-              data-text={props.copiableDate ? undefined : dateText}
-            >
-              {props.copiableDate && props.date && (props.dateString || format(props.date))}
-              {props.status && (
-                <span className='rce-mbox-status'>
-                  {props.status === 'waiting' && <MdAccessTime />}
 
-                  {props.status === 'sent' && <MdCheck />}
-
-                  {props.status === 'received' && <IoIosDoneAll />}
-
-                  {props.status === 'read' && <MdDoneAll color='#4FC3F7' />}
-                </span>
+              {(props.title || props.avatar) && (
+                <div
+                  style={{ ...(props.titleColor && { color: props.titleColor }) }}
+                  onClick={props.onTitleClick}
+                  className={classNames('rce-mbox-title', {
+                    'rce-mbox-title--clear': props.type === 'text',
+                  })}
+                >
+                  {props.avatar && <Avatar letterItem={props.letterItem} src={props.avatar} />}
+                  {props.title && <span>{props.title}</span>}
+                </div>
               )}
+
+              {props.reply && <ReplyMessage onClick={props.onReplyMessageClick} {...props.reply} />}
+
+              {props.type === 'text' && (
+                <div
+                  className={classNames('rce-mbox-text', {
+                    'rce-mbox-text-retracted': props.retracted,
+                    'left': props.position === 'left',
+                    'right': props.position === 'right',
+                  })}
+                >
+                  {props.retracted && <MdBlock />}
+                  {props.text}
+                </div>
+              )}
+
+              {props.type === 'location' && <LocationMessage focus={focus} notch={notch} {...props} />}
+
+              {props.type === 'photo' && <PhotoMessage focus={focus} notch={notch} {...props} />}
+
+              {props.type === 'video' && <VideoMessage focus={focus} notch={notch} {...props} />}
+
+              {props.type === 'file' && <FileMessage focus={focus} notch={notch} {...props} />}
+
+              {props.type === 'spotify' && <SpotifyMessage focus={focus} notch={notch} {...props} />}
+
+              {props.type === 'meeting' && <MeetingMessage focus={focus} notch={notch} {...props} />}
+              {props.type === 'audio' && <AudioMessage focus={focus} notch={notch} {...props} />}
+
+              {props.type === 'meetingLink' && (
+                <MeetingLink focus={focus} notch={notch} {...props} actionButtons={props?.actionButtons} />
+              )}
+
+              <div
+                className={classNames(
+                  'rce-mbox-time',
+                  { 'rce-mbox-time-block': thatAbsoluteTime },
+                  { 'non-copiable': !props.copiableDate }
+                )}
+                data-text={props.copiableDate ? undefined : dateText}
+              >
+                {props.copiableDate && props.date && (props.dateString || format(props.date))}
+                {props.status && (
+                  <span className='rce-mbox-status'>
+                    {props.status === 'waiting' && <MdAccessTime />}
+
+                    {props.status === 'sent' && <MdCheck />}
+
+                    {props.status === 'received' && <IoIosDoneAll />}
+
+                    {props.status === 'read' && <MdDoneAll color='#4FC3F7' />}
+                  </span>
+                )}
+              </div>
             </div>
-          </div>
 
-          {notch &&
-            (props.position === 'right' ? (
-              <svg
-                className={classNames('rce-mbox-right-notch', { 'message-focus': focus })}
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-              >
-                <path d='M0 0v20L20 0' />
-              </svg>
-            ) : (
-              <div>
+            {notch &&
+              (props.position === 'right' ? (
                 <svg
-                  className={classNames('rce-mbox-left-notch', { 'message-focus': focus })}
+                  className={classNames('rce-mbox-right-notch', { 'message-focus': focus })}
                   xmlns='http://www.w3.org/2000/svg'
                   viewBox='0 0 20 20'
                 >
-                  <defs>
-                    <filter id='filter1' x='0' y='0'>
-                      <feOffset result='offOut' in='SourceAlpha' dx='-2' dy='-5' />
-                      <feGaussianBlur result='blurOut' in='offOut' stdDeviation='3' />
-                      <feBlend in='SourceGraphic' in2='blurOut' mode='normal' />
-                    </filter>
-                  </defs>
-                  <path d='M20 0v20L0 0' filter='url(#filter1)' />
+                  <path d='M0 0v20L20 0' />
                 </svg>
-              </div>
-            ))}
-        </div>
-      )}
-    </div>
+              ) : (
+                <div>
+                  <svg
+                    className={classNames('rce-mbox-left-notch', { 'message-focus': focus })}
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 20 20'
+                  >
+                    <defs>
+                      <filter id='filter1' x='0' y='0'>
+                        <feOffset result='offOut' in='SourceAlpha' dx='-2' dy='-5' />
+                        <feGaussianBlur result='blurOut' in='offOut' stdDeviation='3' />
+                        <feBlend in='SourceGraphic' in2='blurOut' mode='normal' />
+                      </filter>
+                    </defs>
+                    <path d='M20 0v20L0 0' filter='url(#filter1)' />
+                  </svg>
+                </div>
+              ))}
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
